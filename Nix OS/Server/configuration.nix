8b48770423822afbbc34a183cab36c72d8a740d0
Vince Maule <vmaule@my.wgu.edu>
2022-08-23 23:23:22 +0000

    Add emulatedSystems

--- Nix OS/Server/configuration.nix
+++ Nix OS/Server/configuration.nix
@@ -11,8 +11,11 @@
     ];
 
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    binfmt.emulatedSystems = ["aarch64-linux"];
+  };
 
   # networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
