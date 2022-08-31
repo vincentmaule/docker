704961060dde96c4ed3a5317baac44b0e580b263
Vince Maule <vmaule@my.wgu.edu>
2022-08-31 21:29:51 +0000

    Add Support for the Latest

--- Nix OS/pi3/sd-img.nix
+++ Nix OS/pi3/sd-img.nix
@@ -3,7 +3,7 @@
   imports = [
     <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64.nix>
   ];
-  boot.kernelPackages = pkgs.linuxPackages_latest; # Raspberry pies have a hard time booting on the LTS kernel.
+  boot.kernelPackages = pkgs.zfs.latestCompatibleLinuxPackages; # Raspberry pies have a hard time booting on the LTS kernel.
   services.openssh = {
     enable = true;
     permitRootLogin = "yes";
