bf703cc536c4974134e9e48395bc6ce438300e5a
Vince Maule <vmaule@my.wgu.edu>
2022-09-11 23:26:41 +0000

    Input User

--- Nix OS/pi3/sd-img.nix
+++ Nix OS/pi3/sd-img.nix
@@ -3,6 +3,16 @@
     <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64-installer.nix>
   ];
 
+  users.users = {
+    shaggy = {
+      password = "test";
+      isNormalUser = true;
+      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+      packages = with pkgs; [
+      ];
+    };
+  };
+
   services.openssh = {
     enable = true;
     permitRootLogin = "yes";
